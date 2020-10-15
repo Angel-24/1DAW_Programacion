2eccc130dc24bdd099f848e79015aab2a5f25a11
Angel-241 <angel2412002@gmail.com>
2020-10-15 14:56:40 +0200

    comenzando ejercicio 05

---
+++ src/tutorialJava/capitulo3/bloque3/Ejercicio05.java
@@ -0,0 +1,35 @@
+package tutorialJava.capitulo3.bloque3;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+
+		int var = 0;
+		int menor = 0;
+		int mayor = 0;
+//		String limite = JOptionPane.showInputDialog("¿Cuántos números se van a introducir?: ");
+		for (int i = 0; i >= 0; i++) {
+			if (var == 0 & i >= 1) {
+				System.out.println(
+						"El mayor número introducido es: " + mayor + ", mientras que el menor es: " + menor + ".");
+				break;
+			}
+			else { if (var != 0 | i == 0) {
+					String str = JOptionPane.showInputDialog("Introduzca un número: ");
+					var = Integer.parseInt(str);
+					if (var < menor) {
+						menor = var;
+					}
+					if (var > mayor) {
+						mayor = var;
+					}
+//					if (menor == 0) {
+//						menor = mayor;
+//					}
+				}
+			}
+		}
+	}
+}
