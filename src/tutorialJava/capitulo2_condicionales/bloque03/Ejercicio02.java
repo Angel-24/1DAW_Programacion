309e7641d7ead19547283055cbdd0c7a177e3bd6
Angel-241 <angel2412002@gmail.com>
2020-10-22 14:44:27 +0200

    C2- B3 - E3

--- src/tutorialJava/capitulo2_condicionales/bloque03/Ejercicio02.java
+++ src/tutorialJava/capitulo2_condicionales/bloque03/Ejercicio02.java
@@ -6,24 +6,50 @@ public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		
+		String str;
 		double num1;
 		double num2;
-		double num3;
+		double resultado;
 		
-		String menu = JOptionPane.showInputDialog("¿Qué tipo de operación va a realizar?");
-		int eleccion = Integer.parseInt(menu);
-		
-		switch (eleccion) {
-		case 1:
-			String str1 = JOptionPane.showInputDialog("Realizar la raíz cuadrada de: ");
-			num1 = Integer.parseInt(str1);
-			Math.sqrt(num1);
+		String menu = JOptionPane.showInputDialog("¿Qué tipo de operación va a realizar?"
+				+ "\n(Raíz, Potencia, División)");
+/*		eleccion = (menu);
+		if (menu == "Raíz") {
+			eleccion = 1;
+		} else {
+			if (menu == "Potencia") {
+				eleccion = 2;
+			} else {
+				if (menu == "División") {
+					eleccion = 3;
+				}
+			}
+		}
+*/
+		switch (menu) {
+		case "Raíz":
+			str = JOptionPane.showInputDialog("Realizar la raíz de: ");
+			num1 = Integer.parseInt(str);
+			str = JOptionPane.showInputDialog("Con exponente: ");
+			num2 = Integer.parseInt(str);
+			resultado = Math.pow(num1, (1 / num2));
+			System.out.println("La raíz cuadrada de " + resultado);
 			break;
-		case 2:
-
+		case "Potencia":
+			str = JOptionPane.showInputDialog("Elevar el número __ ...");
+			num1 = Integer.parseInt(str);
+			str = JOptionPane.showInputDialog("... a la potencia _.");
+			num2 = Integer.parseInt(str);
+			resultado = Math.pow(num1, num2);
+			System.out.println(num1 + "^" + num2 + " = " + resultado);
 			break;
-		case 3:
-//			System.out.println("Una nota de " + puntos + " puntos se califica como 'Muy deficiente'");
+		case "División":
+			str = JOptionPane.showInputDialog("Mostrar el resto obtenido al dividir __ ...");
+			num1 = Integer.parseInt(str);
+			str = JOptionPane.showInputDialog("... entre _.");
+			num2 = Integer.parseInt(str);
+			resultado = num1 % num2;
+			System.out.println("El resto de la operación: " + num1 + " / " + num2 + "es: " + resultado);
 			break;
 		}
 	}
---
+++ src/tutorialJava/capitulo2_condicionales/bloque03/Ejercicio03.java
@@ -0,0 +1,65 @@
+package tutorialJava.capitulo2_condicionales.bloque03;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		int menu1 = 1;
+		String menu = "1";
+		int reiniciar = 0;
+		
+		while (menu1 != 0 && reiniciar == 0) {
+			double num1 = 0;
+			double num2 = 0;
+			double resultado = 0;
+			String str;
+			menu = JOptionPane.showInputDialog("Elige el número del tipo de operación que vas a realizar: "
+					+ "\n1. Hipotenusa de un Triángulo" + "\n2. Superficie de Circunferencia"
+					+ "\n3. Perímetro de Circunferencia" + "\n4. Área de un Rectángulo" + "\n5. Área de un Triángulo");
+
+			switch (menu) {
+			case "1":
+				str = JOptionPane.showInputDialog("Longitud del Cateto A");
+				num1 = Double.parseDouble(str);
+				str = JOptionPane.showInputDialog("Longitud del Cateto B");
+				num2 = Double.parseDouble(str);
+				resultado = (Math.pow(num1, 2)) + (Math.pow(num2, 2));
+				System.out.println("La hipotenusa del triángulo es: " + resultado);
+				break;
+			case "2":
+				str = JOptionPane.showInputDialog("Longitud del radio de la circunferencia");
+				num1 = Double.parseDouble(str);
+				resultado = (Math.pow(num1, 2)) * 3.1416;
+				System.out.println("La superficie de la circunferencia es: " + resultado);
+				break;
+			case "3":
+				str = JOptionPane.showInputDialog("Longitud del radio de la circunferencia");
+				num1 = Double.parseDouble(str);
+				resultado = num1 * (2 * 3.1416);
+				System.out.println("El perímetro de la circunferencia es: " + resultado);
+				break;
+			case "4":
+				str = JOptionPane.showInputDialog("La longitud de la base del rectángulo");
+				num1 = Double.parseDouble(str);
+				str = JOptionPane.showInputDialog("La longitud de la altura del rectángulo");
+				num2 = Double.parseDouble(str);
+				resultado = num1 * num2;
+				System.out.println("El área del rectángulo es: " + resultado);
+				break;
+			case "5":
+				str = JOptionPane.showInputDialog("La longitud de la base del triángulo");
+				num1 = Double.parseDouble(str);
+				str = JOptionPane.showInputDialog("La longitud de la altura del triángulo");
+				num2 = Double.parseDouble(str);
+				resultado = (num1 * num2) / 2;
+				System.out.println("El área del triángulo es: " + resultado);
+				break;
+			case "0":
+				menu1 = 0;
+				break;
+			}
+			reiniciar = JOptionPane.showInternalConfirmDialog(null, "¿Reiniciar?");
+		}
+	}
+}
