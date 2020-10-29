3e2fff009afb29658aa20a26f5c2820650001a0f
Angel-241 <angel2412002@gmail.com>
2020-10-29 14:54:17 +0100

    Iniciando el capitulo 5

--- src/tutorialJava/Utils.java
+++ src/tutorialJava/Utils.java
@@ -2,6 +2,7 @@ package tutorialJava;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -12,4 +13,36 @@ public class Utils {
 		 return (int) Math.round(Math.random() * 100);
 	}
 	
+
+	
+	public static int obtenerNumeroAzar (int min, int max) {
+		 return (int) Math.round(Math.random() * (max - min)) + min;
+	}
+	 
+	
+	
+	public static int obtenerEntero () {
+		int numero = 0;
+		try {
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader (isr);
+			numero = Integer.parseInt (br.readLine());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return numero;
+	}
+
+	
+	
+	public static int obtenerEnteroMet2() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
+	}
+	
+	
+	public static int obtenerEnteroMet3() {
+		String str = JOptionPane.showInputDialog("Introduce un número");
+		return Integer.parseInt(str);
+	}
 }
