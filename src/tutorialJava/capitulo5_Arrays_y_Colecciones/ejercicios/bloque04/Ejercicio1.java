ad27fc55171f8d3ebaa3f9b5a67a571dd76ccb6f
Angel-241 <angel2412002@gmail.com>
2020-11-20 14:10:30 +0100

    .

--- src/tutorialJava/capitulo5_Arrays_y_Colecciones/ejercicios/bloque04/Ejercicio1.java
+++ src/tutorialJava/capitulo5_Arrays_y_Colecciones/ejercicios/bloque04/Ejercicio1.java
@@ -9,23 +9,44 @@ import tutorialJava.UtilsArrays;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
-		int i;
-		for (i = 1; i < 5; i++) {
-		int parametro[i] = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número entero"));
-		float parametro[i] = Float.parseFloat(JOptionPane.showInputDialog("Introduce un número flotante"));
-		}
-		media(parametro);
-		
-		
+		int vector[] = UtilsArrays.creaArrayNumerosAzar(150, 0, 1000);
+
+		UtilsArrays.mostrarArray(vector);
+
+		// Mido el tiempo de inicio
+		long millisAntesDeLaOrdenacion = new Date().getTime();
+
+		ordenaArrayPorBurbuja(vector);
+
+		// Mido el tiempo después
+		long millisDespuesDeLaOrdenacion = new Date().getTime();
+
+		// Muestro el tiempo en millis
+		System.out.println("\nEjecución en " + (millisDespuesDeLaOrdenacion - millisAntesDeLaOrdenacion) + " millis\n");
+
 		// Mostramos el resultado
-		System.out.println(parametro);
+		UtilsArrays.mostrarArray(vector);
 	}
 
-	
-	public static void media(Float[] args) {
-
-		
-	
+	/**
+	 * Implementa el algoritmo de la buburja para ordenar un array
+	 * 
+	 * @param array Array desordenado que se ordenará
+	 */
+	public static void ordenaArrayPorBurbuja(int array[]) {
+		boolean hayIntercambios;
+		do {
+			hayIntercambios = false;
+			// Empieza el algoritmo
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i + 1] < array[i]) {
+					// Entonces hago un intercambio
+					int aux = array[i + 1];
+					array[i + 1] = array[i];
+					array[i] = aux;
+					hayIntercambios = true;
+				}
+			}
+		} while (hayIntercambios);
 	}
 }
