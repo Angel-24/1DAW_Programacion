5fbae0c0950b21925db0fcd9014e392808e5f57c
Angel-241 <angel2412002@gmail.com>
2020-12-03 11:29:27 +0100

    bolos otra vez

--- src/tutorialJava/capitulo5_Arrays_y_Colecciones/ejercicios/bolos/bolos.java
+++ src/tutorialJava/capitulo5_Arrays_y_Colecciones/ejercicios/bolos/bolos.java
@@ -8,70 +8,125 @@ public class bolos {
 
 	public static void main(String[] args) {
 
-		int[] array = new int[1];
-		String intento = "        (<Esc> = salir del programa)\n        (<Espacio> = lanzar la bola)\n\n\n\n\n\n\n\n\n\n\n\n\n\n Primer intento:";
-		int lanzar = 0;
-		int cae = 0;
-		int j;
-		int puntuacion = 0;
 
-		array = creaArrayBolos(10, 1, 1);
+		int puntuacionJ1 = 0;
+		int jugadores = JOptionPane.showConfirmDialog(null,
+				" < Escoge el número de Jugadores >   \n\n                  1 J.       2 J.\n                   ↓          ↓",
+				"Modo de Juego", 0, 2);
 
+		for (int modo = 0; modo <= jugadores; modo++) {
+
+			int[] array = new int[1];
+			String tutorial = null;
+			if (jugadores == 0) {
+				tutorial = "              (<Esc> = Salir del programa)\n              (<Espacio> = Lanzar la bola)\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+			}
+			if (jugadores == 1) {
+				tutorial = "              (<Esc> = Salir del programa)\n              (<Espacio> = Lanzar la bola)\n\n"
+						+ "                 Turno del Jugador 1  \n\n\n\n\n\n\n\n\n\n\n\n";
+			}
+			if (jugadores == 1 && modo == 1) {
+				tutorial = "              (<Esc> = Salir del programa)\n              (<Espacio> = Lanzar la bola)\n\n"
+						+ "                 Turno del Jugador 2  \n\n\n\n\n\n\n\n\n\n\n";
+			}
+			String intento = tutorial;
+			int lanzar = 0;
+			int cae = 0;
+			int j;
+			int puntuacion = 0;
+			
+			array = creaArrayBolos(10, 1, 1);
+			
 //		System.out.println("Primer Intento:");
 
-		mostrarArray(array, intento, puntuacion);
+			mostrarArray(array, intento, puntuacion, tutorial, modo, jugadores, puntuacionJ1);
 
-		for (int i = 0; lanzar == 0 && i < 3; i++) {
+			for (int i = 0; lanzar == 0 && i < 2; i++) {
 
-			if (i == 0) {
-				intento = " Último intento:";
-				lanzar = JOptionPane.showConfirmDialog(null, "    [ Primera Bola ]", "¡Lanzar!", -1, 2);
-			}
-
-			if (i == 1) {
-				intento = " Puntuación:";
-				lanzar = JOptionPane.showConfirmDialog(null, "    [ Segunda Bola ]", "¡Lanzar!", -1, 2);
-			}
-
-			animarArray(array, intento, puntuacion);
-
-			j = 0;
-			for (j = 0; j < 10; j++) {
-
-				cae = probabilidad(array, cae);
-				if (cae == 1 && array[j] == 1) {
-					array[j] = 0;
-					cae = 0;
-					puntuacion = puntuacion + 1;
+				if (i == 0) {
+					intento = " Último intento:";
+					lanzar = JOptionPane.showConfirmDialog(null, "                [  Primera Bola  ]", "¡Lanzar!", -1,
+							-1);
 				}
-			}
 
-			if (lanzar == -1) {
-				break;
-			}
+				if (i == 1) {
+					intento = " Puntuación:";
+					lanzar = JOptionPane.showConfirmDialog(null, "                [  Segunda Bola  ]", "¡Lanzar!", -1,
+							-1);
+				}
+				
 
-			mostrarArray(array, intento, puntuacion);
+				if (jugadores == 1 && modo == 1 && i == 1) {
+					tutorial = "              (<Esc> = Salir del programa)\n              (<Espacio> = Lanzar la bola)\n\n"
+							+ "\n\n\n\n\n\n\n\n\n\n\n";
+				}
+				
+//			animarArray(array, intento, puntuacion);
+				j = 0;
+				for (j = 0; j < 10; j++) {
+
+					cae = probabilidad(array, cae);
+					if (cae == 1 && array[j] == 1) {
+						array[j] = 0;
+						cae = 0;
+						puntuacion = puntuacion + 1;
+						if  (modo == 0 && jugadores == 1) {
+							puntuacionJ1 = puntuacion;
+						}
+					}
+				}
+
+				if (lanzar == -1) {
+					break;
+				}
+				if (i == 1 && modo == 0) {
+					intento = " Puntuación:";
+					lanzar = JOptionPane.showConfirmDialog(null, "          [  El jugador 1 ha terminado su turno  ]        ", "Continuar", -1,
+							-1);
+				}
+				mostrarArray(array, intento, puntuacion, tutorial, modo, jugadores, puntuacionJ1);
+			}
 		}
 	}
 
 //########################################################################################################
 
-	public static void mostrarArray(int[] array, String intento, int puntuacion) {
+	public static void mostrarArray(int[] array, String intento, int puntuacion, String tutorial, int modo,
+		int jugadores, int puntuacionJ1) {
 
 		for (int i = 0; i <= array.length; i++) {
 			if (i == 0) {
-				if (intento == " Puntuación:" && puntuacion >= 10) {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "¡¡¡PLENO!!!"
-							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
-				} else
-				if (intento == " Puntuación:") {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   " + (puntuacion)
-							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				if (intento == " Puntuación:" && puntuacion >= 10 && jugadores == 0) {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial + intento
+							+ "   ¡¡¡PLENO!!!"
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				} else if (intento == " Puntuación:" && puntuacion < 10 && jugadores == 0) {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial + intento + "   "
+							+ (puntuacion)
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
 				}
-				if (intento != " Puntuación:") {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento
-							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				if (intento != " Puntuación:" && jugadores == 0) {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
 				}
+				if (intento == " Puntuación:" && puntuacion >= 10 && jugadores == 1) {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial + intento + "   "
+							+ (puntuacionJ1)  + "  [Jugador 1]" + "\n" + intento
+							+ "   ¡¡¡PLENO!!!" + "  [Jugador 2]"
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				} else if (intento == " Puntuación:" && puntuacion < 10 && jugadores == 1) {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial + intento + "   "
+							+ (puntuacionJ1)  + "  [Jugador 1]" + "\n" + intento + "   "
+									+ (puntuacion) + "  [Jugador 2]"
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				}
+				if (intento != " Puntuación:" && jugadores == 1) {
+					intento = "Puntuación:";
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + tutorial + intento + "   "
+							+ (puntuacionJ1)  + "  [Jugador 1]" + "\n"
+							+ "\n_________________________________________\n ___________________   ______________   _\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
+				}
+
 				System.out.print("| | |#| " + array[i]);
 			}
 			if (i == 1) {
@@ -99,16 +154,11 @@ public class bolos {
 				System.out.print(" " + array[i] + "   |#| | |#|       |#|  | |\n| | |#|   ");
 			}
 			if (i == 9) {
-				System.out.print(" " + array[i]
-						+ "    |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  / /\n"
-						+ "\\_\\_|#|         |#| | |#|       |#|_/_/"
-						+ "\n");
+				System.out.print(" " + array[i] + "    |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  / /\n"
+						+ "\\_\\_|#|         |#| | |#|       |#|_/_/" + "\n");
 			}
 		}
 	}
@@ -118,7 +168,7 @@ public class bolos {
 	public static int probabilidad(int[] array, int cae) {
 
 		int prob = Utils.obtenerNumeroAzar(0, 100);
-		if (prob > 70) {
+		if (prob > 75) {
 			cae = 1;
 		}
 		return cae;
@@ -143,9 +193,9 @@ public class bolos {
 				if (intento == " Puntuación:" && puntuacion >= 10) {
 					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "¡¡¡PLENO!!!"
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
-				} else
-				if (intento == " Puntuación:") {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   " + (puntuacion)
+				} else if (intento == " Puntuación:") {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   "
+							+ (puntuacion)
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
 				}
 				if (intento != " Puntuación:") {
@@ -179,27 +229,22 @@ public class bolos {
 				System.out.print(" " + array[i] + "   |#| | |#|       |#|  | |\n| | |#|   ");
 			}
 			if (i == 9) {
-				System.out.print(" " + array[i]
-						+ "    |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  / /\n"
-						+ "\\_\\_|#|         |#| | |#|       |#|_/_/"
-						+ "\n");
+				System.out.print(" " + array[i] + "    |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  / /\n"
+						+ "\\_\\_|#|         |#| | |#|       |#|_/_/" + "\n");
 			}
 		}
-		
+
 		for (int i = 0; i <= 20000 && i > 10000; i++) {
 			if (i >= 0) {
 				if (intento == " Puntuación:" && puntuacion >= 10) {
 					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "¡¡¡PLENO!!!"
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
-				} else
-				if (intento == " Puntuación:") {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   " + (puntuacion)
+				} else if (intento == " Puntuación:") {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   "
+							+ (puntuacion)
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
 				}
 				if (intento != " Puntuación:") {
@@ -233,27 +278,22 @@ public class bolos {
 				System.out.print(" " + array[i] + "   |#| | |#|       |#|  | |\n| | |#|   ");
 			}
 			if (i == 9) {
-				System.out.print(" " + array[i]
-						+ "    |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  / /\n"
-						+ "\\_\\_|#|         |#| | |#|       |#|_/_/"
-						+ "\n");
+				System.out.print(" " + array[i] + "    |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  / /\n"
+						+ "\\_\\_|#|         |#| | |#|       |#|_/_/" + "\n");
 			}
 		}
-		
+
 		for (int i = 0; i <= 30000 && i < 20000; i++) {
 			if (i >= 0) {
 				if (intento == " Puntuación:" && puntuacion >= 10) {
 					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "¡¡¡PLENO!!!"
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
-				} else
-				if (intento == " Puntuación:") {
-					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   " + (puntuacion)
+				} else if (intento == " Puntuación:") {
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + intento + "   "
+							+ (puntuacion)
 							+ "\n\n ___________________   ________________\n| |\\__  _______  __/| |  _______  __/| |\n| | \\/_/_______\\_\\/ | |_/_______\\_\\/ | |\n| | |#|         |#| | |#|       |#|  | |");
 				}
 				if (intento != " Puntuación:") {
@@ -287,16 +327,11 @@ public class bolos {
 				System.out.print(" " + array[i] + "   |#| | |#|       |#|  | |\n| | |#|   ");
 			}
 			if (i == 9) {
-				System.out.print(" " + array[i]
-						+ "    |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  | |\n"
-						+ "| | |#|         |#| | |#|       |#|  / /\n"
-						+ "\\_\\_|#|         |#| | |#|       |#|_/_/"
-						+ "\n");
+				System.out.print(" " + array[i] + "    |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  | |\n"
+						+ "| | |#|         |#| | |#|       |#|  | |\n" + "| | |#|         |#| | |#|       |#|  / /\n"
+						+ "\\_\\_|#|         |#| | |#|       |#|_/_/" + "\n");
 			}
 		}
 	}
