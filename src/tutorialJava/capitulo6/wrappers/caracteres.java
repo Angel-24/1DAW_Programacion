7750749812c0516c52f26203cd2eafa8624c12b0
Angel-241 <angel2412002@gmail.com>
2021-02-11 14:58:32 +0100

    Caracteres en los wrappers

--- src/tutorialJava/capitulo6/wrappers/caracteres.java
+++ src/tutorialJava/capitulo6/wrappers/caracteres.java
@@ -1,6 +1,12 @@
 package tutorialJava.capitulo6.wrappers;
 
+import java.util.Scanner;
+
 public class caracteres {
+
+	private static String a;
+	private static String palabra;
+	private static int n;
 
 	public caracteres() {
 		// TODO Auto-generated constructor stub
@@ -8,15 +14,54 @@ public class caracteres {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Requisitos: ");
-		System.out.println("Una mayúscula");
-		System.out.println("Una minúscula");
-		System.out.println("Un dígito");
-		System.out.println("Un caracter no alfanumérico");
-		System.out.println("\n Contraseña: \n");
+		for (n=1;n>0;n++) {
 
-		ScannerFactory.instance(a).equals(object);
+			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nIntento Nº" + n);
+			
+			enunciado();
+			
+			escaner();
+			
+		}
+		
+		cerrar();
 		
 	}
 
+	public static void cerrar(){
+		
+	}
+	
+	public static void enunciado(){
+
+		System.out.println("\n   Requisitos: ");
+		System.out.println("·Una mayúscula");
+		System.out.println("·Una minúscula");
+		System.out.println("·Un dígito");
+		System.out.println("·Un caracter no alfanumérico");
+		System.out.println("\n   Contraseña: \n");
+		
+	}
+	
+	public static void escaner() {
+		
+		int condicion = 0;
+
+		Scanner a = new Scanner(System.in);
+		palabra = a.nextLine();
+
+		char cadenaTexto[] = palabra.toCharArray();
+		
+		for (int i = 0; i < cadenaTexto.length; i++) {
+			if (Character.isDigit(cadenaTexto[i]) == true) condicion++;
+			if (Character.isLetter(cadenaTexto[i]) == true) condicion++;
+			if (Character.isLetterOrDigit(cadenaTexto[i]) == false) condicion++;
+			if (Character.isLowerCase(cadenaTexto[i]) == true) condicion++;
+			if (Character.isUpperCase(cadenaTexto[i]) == true) condicion++;
+			
+			System.out.println(condicion);
+		}
+		
+	}
+	
 }
