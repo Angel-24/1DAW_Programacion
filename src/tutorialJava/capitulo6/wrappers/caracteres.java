331c58d4a560b70cdb0314fcb402253f223d5801
Angel-241 <angel2412002@gmail.com>
2021-02-12 14:13:47 +0100

    Terminé lo de las contraseñas y tengo que buscar el método para convertir un entero en código hexadecimal.

--- src/tutorialJava/capitulo6/wrappers/caracteres.java
+++ src/tutorialJava/capitulo6/wrappers/caracteres.java
@@ -7,6 +7,11 @@ public class caracteres {
 	private static String a;
 	private static String palabra;
 	private static int n;
+	private static boolean condicion = false;
+	private static boolean mayuscula = false;
+	private static boolean minuscula = false;
+	private static boolean digito = false;
+	private static boolean simbolo = false;
 
 	public caracteres() {
 		// TODO Auto-generated constructor stub
@@ -14,14 +19,17 @@ public class caracteres {
 
 	public static void main(String[] args) {
 		
-		for (n=1;n>0;n++) {
-
-			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nIntento Nº" + n);
+		for (n=1; condicion==false; n++) {
+			
+			System.out.println("\nIntento Nº" + n);
 			
 			enunciado();
 			
 			escaner();
 			
+			if (condicion == true && n>=1) System.out.println("\n[Nivel de seguridad:  *Válido*]");
+			else if (condicion == false && n>=1) System.out.println("\n[Nivel de seguridad:  *Insuficiente*]\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
 		}
 		
 		cerrar();
@@ -30,36 +38,38 @@ public class caracteres {
 
 	public static void cerrar(){
 		
+		System.out.println("\n[Cerrando el programa...]");
+		
 	}
 	
 	public static void enunciado(){
 
-		System.out.println("\n   Requisitos: ");
+		System.out.println("\n  Requisitos: ");
 		System.out.println("·Una mayúscula");
 		System.out.println("·Una minúscula");
 		System.out.println("·Un dígito");
 		System.out.println("·Un caracter no alfanumérico");
-		System.out.println("\n   Contraseña: \n");
+		System.out.println("\n\n\n   Contraseña: ");
 		
 	}
 	
 	public static void escaner() {
 		
-		int condicion = 0;
-
 		Scanner a = new Scanner(System.in);
 		palabra = a.nextLine();
 
 		char cadenaTexto[] = palabra.toCharArray();
 		
 		for (int i = 0; i < cadenaTexto.length; i++) {
-			if (Character.isDigit(cadenaTexto[i]) == true) condicion++;
-			if (Character.isLetter(cadenaTexto[i]) == true) condicion++;
-			if (Character.isLetterOrDigit(cadenaTexto[i]) == false) condicion++;
-			if (Character.isLowerCase(cadenaTexto[i]) == true) condicion++;
-			if (Character.isUpperCase(cadenaTexto[i]) == true) condicion++;
 			
-			System.out.println(condicion);
+//			if (Character.isLetter(cadenaTexto[i]) == true) condicion++;
+			if (Character.isDigit(cadenaTexto[i]) == true) digito=true;
+			if (Character.isLetterOrDigit(cadenaTexto[i]) == false) simbolo=true;
+			if (Character.isLowerCase(cadenaTexto[i]) == true) minuscula=true;
+			if (Character.isUpperCase(cadenaTexto[i]) == true) mayuscula=true;
+			
+			if (digito && simbolo && minuscula && mayuscula) condicion = true;
+
 		}
 		
 	}
---
+++ src/tutorialJava/capitulo6/wrappers/numeros.java
@@ -0,0 +1,19 @@
+package tutorialJava.capitulo6.wrappers;
+
+public class numeros {
+
+	private static int n = 1;
+
+	public numeros() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		for (;n <= 100; n++) {
+		
+			String hex[] = ;
+		
+		}
+	}
+}
